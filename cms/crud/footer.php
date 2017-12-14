b7c69fa8da53fce2d8295926ca9c13148337305b
Artem Works <hello@artemworks.com>
2017-12-14 12:55:14 +0200

    minor changes

--- cms/crud/footer.php
+++ cms/crud/footer.php
@@ -6,16 +6,16 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <p>&copy <a href="<?= $git_url ?>"><?= $cms_title ?></a></p>
+                    <p>&copy 2017 <a href="<?= $git_url ?>"><?= $cms_title ?></a></p>
                 </div>
             </div>
         </div>
     </footer>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="<?= $dir_url ?>assets/js/bootstrap.min.js"></script>
-    <script src="<?= $dir_url ?>cms/vendors/summernote/summernote-bs4.js"></script>
+    <script src="<?= DIR_URL ?>assets/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="<?= DIR_URL ?>assets/js/popper.min.js"></script>
+    <script src="<?= DIR_URL ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?= DIR_URL ?>cms/vendors/summernote/summernote-bs4.js"></script>
 
     <script type="text/javascript">
     function readCover(input) {
--- footer.php
+++ footer.php
@@ -6,15 +6,15 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <p>&copy <a href="<?= $git_url ?>"><?= $cms_title ?></a></p>
+                    <p>&copy 2017 <a href="<?= $git_url ?>"><?= $cms_title ?></a></p>
                 </div>
             </div>
         </div>
     </footer>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="<?= DIR_URL ?>assets/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="<?= DIR_URL ?>assets/js/popper.min.js"></script>
+    <script src="<?= DIR_URL ?>assets/js/bootstrap.min.js"></script>
 
   </body>
 </html>
